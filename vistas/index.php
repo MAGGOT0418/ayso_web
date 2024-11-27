31e75a9b8a8fc1fb3a02d33201e6334f0052cbb4
Adan0suna <gabrielosuna240@gmail.com>
2024-11-27 09:20:11 -0600

    adan
    1

--- vistas/index.php
+++ vistas/index.php
@@ -21,6 +21,7 @@ if (isset($_SESSION['id_usuario'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/footer.css">
 </head>
 
 <body>
